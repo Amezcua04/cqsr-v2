0f1369653ecfc674edb5c1ac49e08b817e451c18
CarlosAmezcua <Amezcua041196@gmail.com>
2024-06-21 14:17:24 -0700

    FIXED: iconos de componente de servicios

---
+++ src/data/data.js
@@ -0,0 +1,533 @@
+// Home
+import Maternidad from "../assets/icons/Maternidad.png";
+import Medicos from "../assets/icons/Medicos.png";
+import Servicios from "../assets/icons/Servicios.png";
+import Membresia from "../assets/icons/Membresia.png";
+import imagen1 from "../assets/Home/imagen1.png";
+import imagen2 from "../assets/Home/imagen2.png";
+import imagen3 from "../assets/Home/imagen3.png";
+import imagen4 from "../assets/Home/imagen4.png";
+import imagen5 from "../assets/Home/imagen5.png";
+import imagen6 from "../assets/Home/imagen6.png";
+
+// Servicios
+import Imagenologia from "../assets/Servicios/Recurso 1.png"
+import Urgencias from "../assets/Servicios/Recurso 2.png";
+import Laboratorio from "../assets/Servicios/Recurso 3.png";
+import Hospitalizacion from "../assets/Servicios/Recurso 4.png";
+import Intermedia from "../assets/Servicios/Recurso 5.png";
+import Traslados from "../assets/Servicios/Recurso 6.png";
+import Cirugia from "../assets/Servicios/Recurso 7.png";
+import Qx from "../assets/Servicios/Recurso 8.png";
+import Maternidad2 from "../assets/Servicios/Recurso 9.png";
+import Tamiz from "../assets/Servicios/Recurso 10.png";
+import Cuidados from "../assets/Servicios/Recurso 11.png";
+import ConsultaExterna from "../assets/Servicios/Recurso 12.png";
+import Esterilizacion from "../assets/Servicios/Recurso 13.png";
+import Farmacia from "../assets/Servicios/Recurso 14.png";
+import Endoscopia from "../assets/Servicios/Recurso 15.png";
+import Oftamica from "../assets/Servicios/Recurso 16.png";
+import Marcapasos from "../assets/Servicios/Recurso 17.png";
+import Mapeo from "../assets/Servicios/Recurso 18.png";
+import Holter from "../assets/Servicios/Recurso 19.png";
+import Shock from "../assets/Servicios/Recurso 20.png";
+
+// Especialidades
+import example from "../assets/icons/Medicos.png";
+
+// Data Home
+export const dataHome = [
+  {
+    titulo: "Paquete de Maternidad",
+    subtitulo:
+      "Reciba a su bebé en un espacio totalmente seguro, con la mejor atención las 24 horas.",
+    url: "#",
+    image: Maternidad,
+  },
+  {
+    titulo: "Médicos Especialistas",
+    subtitulo:
+      "Amplio staff de médicos especialistas dedicados al cuidado de su salud.",
+    url: "#",
+    image: Medicos,
+  },
+  {
+    titulo: "Servicios Médicos",
+    subtitulo:
+      "Más de 20 servicios médicos con tecnología de punta a su disposición.",
+    url: "#",
+    image: Servicios,
+  },
+  {
+    titulo: "Membresía Salud365",
+    subtitulo:
+      "Nuestra primera membresía de salud con grandes beneficios para toda la familia.",
+    url: "#",
+    image: Membresia,
+  },
+];
+
+// Items Home
+export const itemsHome = [
+  {
+    url: imagen1,
+  },
+  {
+    url: imagen2,
+  },
+  {
+    url: imagen3,
+  },
+  {
+    url: imagen4,
+  },
+  {
+    url: imagen5,
+  },
+  {
+    url: imagen6,
+  },
+];
+
+
+// Data Servicios
+export const dataServicios = [
+  {
+    titulo: "Centro de Imagenología",
+    subtitulo: "Servicio de Tomografía y Rayos X las 24 horas.",
+    url: "#",
+    image: Imagenologia,
+  },
+  {
+    titulo: "Urgencias las 24 horas",
+    subtitulo: "Atención médica oportuna, profesional y ética.",
+    url: "#",
+    image: Urgencias,
+  },
+  {
+    titulo: "Laboratorio las 24 horas",
+    subtitulo:
+      "Estudios de rutina y especiales con entrega de resultados hasta en 1 hora.",
+    url: "#",
+    image: Laboratorio,
+  },
+  {
+    titulo: "Hospitalización",
+    subtitulo:
+      "Espacios seguros y cómodos, con una atención de calidad y calidez.",
+    url: "#",
+    image: Hospitalizacion,
+  },
+  {
+    titulo: "Terapia Intermedia",
+    subtitulo: "Cuidado especializado para los pacientes en condiciones críticas.",
+    url: "#",
+    image: Intermedia,
+  },
+  {
+    titulo: "Traslados programados",
+    subtitulo:
+      "Modernas unidades equipadas para el traslados seguros dentro y fuera de la ciudad.",
+    url: "#",
+    image: Traslados,
+  },
+  {
+    titulo: "Cirugía HoLEP",
+    subtitulo:
+      "Equipo de vanguardia para el tratamiento de padecimientos de la próstata.",
+    url: "#",
+    image: Cirugia,
+  },
+  {
+    titulo: "Quirófanos",
+    subtitulo:
+      "Salas de quirófano con tecnología de punta para la realización de cirugías.",
+    url: "#",
+    image: Qx,
+  },
+  {
+    titulo: "Paquetes de maternidad",
+    subtitulo:
+      "Reciba a su bebé en un espacio totalmente seguro, con la mejor atención.",
+    url: "#",
+    image: Maternidad2,
+  },
+  {
+    titulo: "Tamiz Neonatal",
+    subtitulo:
+      "Estudios con detección de hasta 140 enfermedades en los recién nacidos.",
+    url: "#",
+    image: Tamiz,
+  },
+  {
+    titulo: "Cuidados Intensivos Neonatales",
+    subtitulo: "Cuidado especializado para los recién nacidos.",
+    url: "#",
+    image: Cuidados,
+  },
+  {
+    titulo: "Consulta externa",
+    subtitulo:
+      "Salas de quirófano con tecnología de punta para la realización de cirugías.",
+    url: "#",
+    image: ConsultaExterna,
+  },
+  {
+    titulo: "Esterilización de material médico",
+    subtitulo: "Esterilización por plasma, vapor y gas.",
+    url: "#",
+    image: Esterilizacion,
+  },
+  {
+    titulo: "Farmacia",
+    subtitulo:
+      "Amplia variedad de medicamentos, con servicio las 24 horas.",
+    url: "#",
+    image: Farmacia,
+  },
+  {
+    titulo: "Sala de endoscopia",
+    subtitulo:
+      "Servicio de endoscopia y colonoscopia por médicos especialistas",
+    url: "#",
+    image: Endoscopia,
+  },
+  {
+    titulo: "Oftálmica",
+    subtitulo:
+      "Detección y tratamiento de padecimientos oculares por médicos especialista.",
+    url: "#",
+    image: Oftamica,
+  },
+  {
+    titulo: "Marcapasos temporal",
+    subtitulo:
+      "Tratamiento urgente y temporal para pacientes con problemas cardíacos.",
+    url: "#",
+    image: Marcapasos,
+  },
+  {
+    titulo: "Mapeo de la presión arterial",
+    subtitulo:
+      "Detección y tratamiento de padecimientos oculares por médicos especialista.",
+    url: "#",
+    image: Mapeo,
+  },
+  {
+    titulo: "Holter",
+    subtitulo:
+      "Reciba a su bebé en un espacio totalmente seguro, con la mejor atención las 24 horas.",
+    url: "#",
+    image: Holter,
+  },
+  {
+    titulo: "Área de shock",
+    subtitulo:
+      "Equipo de vanguardia para el tratamiento de padecimientos de la próstata.",
+    url: "#",
+    image: Shock,
+  },
+];
+
+// especialidades
+
+export const categorias = [
+  "Alergología",
+  "Cardiólogía",
+  "Cirugía general",
+  "Coloproctología",
+  "Endocrinología",
+  "Gastroenterología",
+  "Geriatría Internista",
+  "Ginecología y obstetricia",
+  "Histopatología",
+  "Medicina interna",
+  "Nefrología",
+  "Neurología",
+  "Oftalmología",
+  "Otorrinolaringología",
+  "Pediatría",
+  "Radiología",
+  "Traumatología y ortopedia",
+  "Urología",
+];
+
+  
+export const dataEspecialistas = [
+  {
+      "imagen": example,
+      "especialidad":"Alergología",
+      "nombre":"Dr. Jaime Díaz Valdivia",
+      "DGPE":10508493
+  },
+  {
+      "imagen": example,
+      "especialidad":"Cardiólogía",
+      "nombre":"Dr. Ricardo Sahagún Sánchez",
+      "DGPE":3393890
+  },
+  {
+      "imagen": example,
+      "especialidad":"Cardiólogía",
+      "nombre":"Dr. Kristian Javier Navarrete Becerra",
+      "DGPE":11650272
+  },
+  {
+      "imagen": example,
+      "especialidad":"Cardiólogía",
+      "nombre":"Dr. Enrique Ignacio Soltero Martínez",
+      "DGPE":"3166505 | 3270201"
+  },
+  {
+      "imagen": example,
+      "especialidad":"Cirugía general",
+      "nombre":"Dr. Miguel Calleros Mariscal",
+      "DGPE":4860278
+  },
+  {
+      "imagen": example,
+      "especialidad":"Cirugía general",
+      "nombre":"Dr. Edwin Omar Fragoza Aguillares",
+      "DGPE":9290716
+  },
+  {
+      "imagen": example,
+      "especialidad":"Cirugía general",
+      "nombre":"Dr. Francisco Javier Muniz Coronado",
+      "DGPE":7046221
+  },
+  {
+      "imagen": example,
+      "especialidad":"Coloproctología",
+      "nombre":"Dra. Alma Rosa Medina Fong",
+      "DGPE":"5848289 | 5212348 "
+  },
+  {
+      "imagen": example,
+      "especialidad":"Endocrinología",
+      "nombre":"Dra. Janet Lizeth Vargas Cortés",
+      "DGPE":11028016
+  },
+  {
+      "imagen": example,
+      "especialidad":"Gastroenterología",
+      "nombre":"Dr. Efraín Magaña Salas",
+      "DGPE":5052284
+  },
+  {
+      "imagen": example,
+      "especialidad":"Gastroenterología",
+      "nombre":"Dr. Gilberto de Jesús Parra Torres",
+      "DGPE":10031185
+  },
+  {
+      "imagen": example,
+      "especialidad":"Geriatría Internista",
+      "nombre":"Dr. Gilberto Medina Fong",
+      "DGPE":"6745642 | 7352969"
+  },
+  {
+      "imagen": example,
+      "especialidad":"Geriatría Internista",
+      "nombre":"Dr. Luis Antonio Flores Ruvalcaba",
+      "DGPE":9739415
+  },
+  {
+      "imagen": example,
+      "especialidad":"Ginecología y obstetricia",
+      "nombre":"Dr. Héctor Leonardo Torres Soltero",
+      "DGPE":3277271
+  },
+  {
+      "imagen": example,
+      "especialidad":"Ginecología y obstetricia",
+      "nombre":"Dr. Ramón González Bobadilla",
+      "DGPE":6323357
+  },
+  {
+      "imagen": example,
+      "especialidad":"Histopatología",
+      "nombre":"Dra. Hilda Rocio Andrade Madrid",
+      "DGPE":6525369
+  },
+  {
+      "imagen": example,
+      "especialidad":"Medicina interna",
+      "nombre":"Dr. José Antonio Carrillo Segura",
+      "DGPE":null
+  },
+  {
+      "imagen": example,
+      "especialidad":"Nefrología",
+      "nombre":"Dra. Elia Judith Corona Galván",
+      "DGPE":6673326
+  },
+  {
+      "imagen": example,
+      "especialidad":"Neurología",
+      "nombre":"Dr. Antonio Durán Ortega",
+      "DGPE":3270331
+  },
+  {
+      "imagen": example,
+      "especialidad":"Neurología",
+      "nombre":"Dr. Mario Gabriel Guevara Barraza",
+      "DGPE":4700291
+  },
+  {
+      "imagen": example,
+      "especialidad":"Neurología",
+      "nombre":"Dr. José Alberto Cruz Cruz",
+      "DGPE":11055456
+  },
+  {
+      "imagen": example,
+      "especialidad":"Neurología",
+      "nombre":"Dr. Ricardo Sahagun Topete",
+      "DGPE":6591998
+  },
+  {
+      "imagen": example,
+      "especialidad":"Oftalmología",
+      "nombre":"Dr. Roberto Diaz Cavazos",
+      "DGPE":4862438
+  },
+  {
+      "imagen": example,
+      "especialidad":"Oftalmología",
+      "nombre":"Dra. Eloísa Platt Soberanes",
+      "DGPE":4559102
+  },
+  {
+      "imagen": example,
+      "especialidad":"Otorrinolaringología",
+      "nombre":"Dra. María del Rocío Zambrano Carrillo",
+      "DGPE":30070
+  },
+  {
+      "imagen": example,
+      "especialidad":"Otorrinolaringología",
+      "nombre":"Dra. Rocío Yáñez Zambrano",
+      "DGPE":10727583
+  },
+  {
+      "imagen": example,
+      "especialidad":"Otorrinolaringología",
+      "nombre":"Dr. José Miguel Plascencia Ramírez",
+      "DGPE":8931464
+  },
+  {
+      "imagen": example,
+      "especialidad":"Pediatría",
+      "nombre":"Dra. Silvia Rodríguez Jiménez",
+      "DGPE":11460626
+  },
+  {
+      "imagen": example,
+      "especialidad":"Pediatría",
+      "nombre":"Dra. Pricila Muro",
+      "DGPE":null
+  },
+  {
+      "imagen": example,
+      "especialidad":"Pediatría",
+      "nombre":"Dra. Ana Lilia Rodríguez de la Peña",
+      "DGPE":"AECEN-25061"
+  },
+  {
+      "imagen": example,
+      "especialidad":"Pediatría",
+      "nombre":"Dra. Elizabeth López Chávez",
+      "DGPE":1815649
+  },
+  {
+      "imagen": example,
+      "especialidad":"Pediatría",
+      "nombre":"Dr. Miguel Licea Pérez Peña",
+      "DGPE":26600
+  },
+  {
+      "imagen": example,
+      "especialidad":"Pediatría",
+      "nombre":"Dra. Evelyn Selene Cortés Salvio",
+      "DGPE":11540617
+  },
+  {
+      "imagen": example,
+      "especialidad":"Pediatría",
+      "nombre":"Dr. Alejandro Solano",
+      "DGPE":null
+  },
+  {
+      "imagen": example,
+      "especialidad":"Pediatría",
+      "nombre":"Dr. Miguel Hernández García",
+      "DGPE":null
+  },
+  {
+      "imagen": example,
+      "especialidad":"Pediatría",
+      "nombre":"Dra. Roxana López Loza",
+      "DGPE":null
+  },
+  {
+      "imagen": example,
+      "especialidad":"Pediatría",
+      "nombre":"Dr. Joctan Esparza Alvarado",
+      "DGPE":null
+  },
+  {
+      "imagen": example,
+      "especialidad":"Pediatría",
+      "nombre":"Dr. Jesús Romero Rodríguez",
+      "DGPE":null
+  },
+  {
+      "imagen": example,
+      "especialidad":"Radiología",
+      "nombre":"Dr. José Alberto Gutiérrez Mata",
+      "DGPE":"AECEM-17241"
+  },
+  {
+      "imagen": example,
+      "especialidad":"Traumatología y ortopedia",
+      "nombre":"Dr. Alejandro Ávalos Flores",
+      "DGPE":6167498
+  },
+  {
+      "imagen": example,
+      "especialidad":"Traumatología y ortopedia",
+      "nombre":"Dr. Jorge García Valencia",
+      "DGPE":6745838
+  },
+  {
+      "imagen": example,
+      "especialidad":"Traumatología y ortopedia",
+      "nombre":"Dr. Luis Ernesto Hernández Rico",
+      "DGPE":5687069
+  },
+  {
+      "imagen": example,
+      "especialidad":"Traumatología y ortopedia",
+      "nombre":"Dr. José Carlos del Real Martínez",
+      "DGPE":11280294
+  },
+  {
+      "imagen": example,
+      "especialidad":"Traumatología y ortopedia",
+      "nombre":"Dr. José Benito del Real Martínez",
+      "DGPE":12187078
+  },
+  {
+      "imagen": example,
+      "especialidad":"Traumatología y ortopedia",
+      "nombre":"Dr. Sealtiel Alfonso Villeda Gómez",
+      "DGPE":11783462
+  },
+  {
+      "imagen": example,
+      "especialidad":"Urología",
+      "nombre":"Dr. Christopher Peña George",
+      "DGPE":6525364
+  }
+];
